e8a672fe6af9722b1c977e9ce599d3dca5555f7f
issey-sun <76093901+issey-sun@users.noreply.github.com>
2021-01-08 19:58:19 +0900

    商品詳細表示機能提出３回目

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'items', to: 'items#index'
   root "items#index"
-  resources :items, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+  resources :items, only: [:index, :new, :create, :show]
 end
+
+# :edit, :update, :destroy
